f6b210c7d694418728b588ba71e03669ff44d868
Fedor <leonenko22222@mail.ru>
2024-05-06 15:56:16 +0500

    second commit

--- precode.sh
+++ precode.sh
@@ -9,8 +9,7 @@ mkdir -p task/dir1 task/dir2 task/dir3/dir4
 # изменяем текущую директорию на task
 cd task
 # создаём пустой файл task/dir2/empty
-cd dir2
-touch empty.txt
+touch dir2/empty.txt
 # создаём файл task/dir2/hello.sh с таким содержанием:
 # #!/bin/bash
 # echo "$1, привет!"
@@ -19,9 +18,8 @@ echo "$1, привет!"' > hello.sh
 # устанавливаем для task/dir2/hello.sh права rwxrw-r--
 chmod 764 hello.sh
 # сохраняем список файлов task/dir2 в task/dir2/list.txt
-ls | touch list.txt
+ls dir2 > dir2/list.txt
 # копируем содержимое каталога task/dir2 в каталог task/dir3/dir4
-cd ..
 cp dir2/* dir3/dir4
 # записываем в task/dir1/summary.txt список файлов с расширением *.txt
 # находящихся в task, включая поддиректории
@@ -32,7 +30,7 @@ cat ./dir2/list.txt >> ./dir2/list.txt
 NAME='Всем студентам'
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
-bash ./dir2/hello.sh "$NAME" >> ./dir1/summary.txt
+./dir2/hello.sh "$NAME" >> dir1/summary.txt
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 mv dir1/summary.txt ./'Практическое задание'
 # выводим на консоль содержимое файла task/Практическое задание
